e0e264bc074d45940bee18322f665fe9ccd0e4ad
shamshinmax <mksmsmsn@gmail.com>
2022-01-28 16:39:10 +0300

    correction fila Task and README

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,8 +1,8 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
-int count = 0;
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
+    int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
